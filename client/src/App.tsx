9b56a9e33b451e29ff75dc77c62b1e0bb8ee30ca
horus <h.horace0921@gmail.com>
2022-10-11 21:41:56 +1100

    git init

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <p className="nes-balloon from-left nes-pointer">
+        This is not a clickable element, but it's an area of the pointer.
+      </p>
+    </div>
+  );
+}
+
+export default App;
