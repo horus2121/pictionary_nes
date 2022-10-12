c2ec1280cb4405802db6a9a0c097d07ab760a18e
horus <h.horace0921@gmail.com>
2022-10-12 23:20:22 +1100

    canvas init

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 
-function App() {
+import { Canvas } from './components/Canvas';
+
+export const App = () => {
   return (
-    <div className="App">
-      <p className="nes-balloon from-left nes-pointer">
-        This is not a clickable element, but it's an area of the pointer.
-      </p>
-    </div>
+    <Canvas />
   );
 }
-
-export default App;
---
+++ client/src/components/Canvas.tsx
@@ -0,0 +1,70 @@
+import { useEffect, useRef, useState } from "react";
+
+const penCursor = "url('https://img.icons8.com/ios-glyphs/30/undefined/quill-pen.png') 0 30, auto"
+
+export const Canvas = () => {
+    const canvasRef = useRef<HTMLCanvasElement>(null)
+    const ctxRef = useRef<HTMLCanvasElement>(null)
+    const [isDrawing, setIsDrawing] = useState(false)
+
+
+    useEffect(() => {
+        if (!canvasRef.current) return
+        const canvas = canvasRef.current
+        const ctx = canvas.getContext('2d')
+
+        canvas.height = window.innerHeight * 0.5
+        canvas.width = window.innerWidth * 0.5
+
+        if (!ctx) return
+        ctx.lineCap = "round"
+        ctx.strokeStyle = "black"
+        ctx.lineWidth = 2
+        canvasRef.current.style.cursor = penCursor
+    }, []
+    )
+
+    const startDrawing = ({ nativeEvent }: any) => {
+        const { clientX, clientY, target: { offsetLeft, offsetTop } } = nativeEvent
+
+        if (!canvasRef.current) return
+        const canvas = canvasRef.current
+        const ctx = canvas.getContext('2d')
+
+        if (!ctx) return
+        ctx.beginPath()
+        ctx.moveTo(clientX - offsetLeft, clientY - offsetTop)
+        setIsDrawing(true)
+    }
+
+    const endDrawing = () => {
+        if (!canvasRef.current) return
+        const canvas = canvasRef.current
+        const ctx = canvas.getContext('2d')
+
+        if (!ctx) return
+        ctx.closePath()
+        setIsDrawing(false)
+    }
+
+    const draw = ({ nativeEvent }: any) => {
+        if (!isDrawing) return
+
+        const { clientX, clientY, target: { offsetLeft, offsetTop } } = nativeEvent
+
+        if (!canvasRef.current) return
+        const canvas = canvasRef.current
+        const ctx = canvas.getContext('2d')
+
+        if (!ctx) return
+        ctx.lineTo(clientX - offsetLeft, clientY - offsetTop)
+        ctx.stroke()
+    }
+    return (
+        <div className="nes-container with-title is-centered">
+            <p className="title">Container.is-centered</p>
+            <p>Good morning. Thou hast had a good night's sleep, I hope.</p>
+            <canvas></canvas>
+        </div>
+    )
+}
